5319a148391c1f607eb5a2aba0ebd16214e6f452
Ray Hung <hungk901@newschool.edu>
2015-09-22 15:32:01 -0400

    update folder name

---
+++ Week3_Branches/Branches/src/Branches.cpp
@@ -0,0 +1,9 @@
+//
+//  Branches.cpp
+//  Branches
+//
+//  Created by Ray_Hung on 9/22/15.
+//
+//
+
+#include "Branches.hpp"
---
+++ Week3_Branches/Branches/src/Branches.hpp
@@ -0,0 +1,14 @@
+//
+//  Branches.hpp
+//  Branches
+//
+//  Created by Ray_Hung on 9/22/15.
+//
+//
+
+#ifndef Branches_hpp
+#define Branches_hpp
+
+#include <stdio.h>
+
+#endif /* Branches_hpp */
